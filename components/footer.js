0eee0181ae27e95a11b1e9bd6527a2d0330deaff
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-20 15:33:30 -0600

    added footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,44 @@
-import React from "react";
+import myData from "@/data";
+import { ButtonGroup, Container, HStack, IconButton, Text } from "@chakra-ui/react";
+import { FaLinkedin, FaGithubAlt, FaEye } from "react-icons/fa";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Container as="footer" role="contentinfo">
+      <HStack justify="space-between" align="center">
+        <Text fontSize="sm">
+          &copy; {new Date().getFullYear()} Geo Sauer, All rights reserved.{" "}
+        </Text>
+        <ButtonGroup variant="">
+          <IconButton
+            as="a"
+            href={myData.socialUrls.linkedIn}
+            aria-label="LinkedIn"
+            data-toggle="tooltip"
+            title="LinkedIn"
+            target="_blank"
+            icon={<FaLinkedin />}
+          />
+          <IconButton
+            as="a"
+            href={myData.socialUrls.github}
+            aria-label="GitHub"
+            data-toggle="tooltip"
+            title="GitHub"
+            target="_blank"
+            icon={<FaGithubAlt />}
+          />
+          <IconButton
+            as="a"
+            href={myData.socialUrls.portfolio}
+            aria-label="Portfolio"
+            data-toggle="tooltip"
+            title="Portfolio"
+            target="_blank"
+            icon={<FaEye />}
+          />
+        </ButtonGroup>
+      </HStack>
+    </Container>
+  );
 }
