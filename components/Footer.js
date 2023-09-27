fb6c5888d7c0a6ff8ae8cbc91a2c8907da76698e
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-27 13:56:12 -0600

    styled Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,43 +1,57 @@
 import myData from "@/data";
-import { ButtonGroup, Container, HStack, IconButton, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  ButtonGroup,
+  Container,
+  Divider,
+  HStack,
+  IconButton,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
 import { FaLinkedin, FaGithubAlt, FaEye } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <Container as="footer" role="contentinfo" minWidth="90vw" paddingTop={10}>
-      <HStack justify="space-between" align="center">
-        <Text fontSize="sm">&copy; {new Date().getFullYear()} Geo Sauer, All rights reserved.</Text>
+    <Box _dark={{ backgroundColor: "gray.900" }} bg={"gray.100"}>
+      <Divider maxW={"6xl"} mx={"auto"} sx={{ borderWidth: "1px" }} />
+      <Container as="footer" role="contentinfo" maxW={"6xl"} paddingTop={10} pb={70}>
+        <HStack justify="space-between" align="center">
+          <Text fontSize="sm">
+            &copy; {new Date().getFullYear()} Geo Sauer, All rights reserved.
+          </Text>
 
-        <ButtonGroup variant="solid">
-          <Tooltip label="LinkedIn">
-            <IconButton
-              as="a"
-              href={myData.socialUrls.linkedIn}
-              aria-label="LinkedIn"
-              target="_blank"
-              icon={<FaLinkedin />}
-            />
-          </Tooltip>
-          <Tooltip label="GitHub">
-            <IconButton
-              as="a"
-              href={myData.socialUrls.github}
-              aria-label="GitHub"
-              target="_blank"
-              icon={<FaGithubAlt />}
-            />
-          </Tooltip>
-          <Tooltip label="Portfolio">
-            <IconButton
-              as="a"
-              href={myData.socialUrls.portfolio}
-              aria-label="Portfolio"
-              target="_blank"
-              icon={<FaEye />}
-            />
-          </Tooltip>
-        </ButtonGroup>
-      </HStack>
-    </Container>
+          <ButtonGroup variant="solid">
+            <Tooltip label="LinkedIn">
+              <IconButton
+                as="a"
+                href={myData.socialUrls.linkedIn}
+                aria-label="LinkedIn"
+                target="_blank"
+                icon={<FaLinkedin />}
+              />
+            </Tooltip>
+            <Tooltip label="GitHub">
+              <IconButton
+                as="a"
+                href={myData.socialUrls.github}
+                aria-label="GitHub"
+                target="_blank"
+                icon={<FaGithubAlt />}
+              />
+            </Tooltip>
+            <Tooltip label="Portfolio">
+              <IconButton
+                as="a"
+                href={myData.socialUrls.portfolio}
+                aria-label="Portfolio"
+                target="_blank"
+                icon={<FaEye />}
+              />
+            </Tooltip>
+          </ButtonGroup>
+        </HStack>
+      </Container>
+    </Box>
   );
 }
