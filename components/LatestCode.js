ca5debafe4caf05d4ad263498f4fc27c2352adc1
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-28 13:17:17 -0600

    fixed weird spacing

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -58,7 +58,7 @@ export default function LatestCode({ repositories }) {
           </Link>
         </Flex>
       </Container>
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={8} px={10} maxWidth={"5xl"} mx={"auto"}>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={20} px={10} maxWidth={"5xl"} mx={"auto"}>
         {repositories &&
           repositories.map((latestRepo, idx) => (
             <GithubRepoCard key={idx} latestRepo={latestRepo} />
