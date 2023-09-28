340e3964342340c6cca94076b58e899bede61ea3
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-27 19:15:26 -0600

    removed data from gitignore

---
+++ data.js
@@ -0,0 +1,45 @@
+const myData = {
+  name: "Geo Sauer",
+  githubUsername: "GeoSauer",
+  designation: "Full-Stack Engineer",
+  avatarUrl: "/avatar.png",
+  email: "geosauer89@gmail.com",
+  address: "Loveland, Colorado, USA",
+  featuredProject: {
+    title: "Grimoire for the Modern Mage",
+    link: "https://grimoire-for-the-modern-mage.netlify.app/welcome",
+    imageUrl: "/gmm-walkthrough.webp",
+  },
+  projects: [
+    {
+      title: "Grimoire for the Modern Mage",
+      link: "https://grimoire-for-the-modern-mage.netlify.app/welcome",
+      imageUrl: "/avatar.png",
+    },
+    {
+      title: "Geortfolio",
+      link: "https://github.com/GeoSauer/geortfolio-next",
+      imageUrl: "/avatar.png",
+    },
+    {
+      title: "Carpe DnDiem",
+      link: "https://github.com/GeoSauer/carpe-dnDiem",
+      imageUrl: "/avatar.png",
+    },
+  ],
+  about: {
+    title: "TBD",
+    desc: [`Like this`, `for`, `line breaks`],
+    currentProject: "TBD",
+    currentProjectUrl: "TBD",
+  },
+  experience: [{ title: "", company: "", year: "", companyUrl: "", desc: "" }],
+  resumeUrl: "",
+  socialUrls: {
+    linkedIn: "https://www.linkedin.com/in/geosauer/",
+    github: "https://github.com/GeoSauer",
+    portfolio: "https://geosauer.com/",
+  },
+};
+
+export default myData;
