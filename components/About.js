9d7da73af945e778be77946d7fe7f6dcc39a3efa
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-28 16:15:23 -0600

    added RainbowUnderline and started building About

--- components/About.js
+++ components/About.js
@@ -1,12 +1,33 @@
-import { Container, Flex, useColorModeValue } from "@chakra-ui/react";
+import myData from "@/data";
+import { Box, Container, Heading, Link, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
+import { RainbowUnderline } from "./RainbowUnderline";
+import { RoughNotationGroup } from "react-rough-notation";
 
 export default function About() {
   const bg = useColorModeValue("gray.100", "gray.900");
 
   return (
-    <Container>
-      <Flex></Flex>
-    </Container>
+    <Box as="section" bg={bg}>
+      <Container maxWidth={"6xl"}>
+        <Heading fontSize={{ base: "6xl", md: "9xl" }} mb={{ base: 20, md: 5 }}>
+          About me.
+        </Heading>
+        <Text as={"p"} fontSize={{ base: "3xl", md: "4xl" }} fontWeight={"semibold"}>
+          {myData.about.title} Currently working on{" "}
+          <Link
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            <RoughNotationGroup show={true}>
+              <RainbowUnderline color={myData.colors[0]}>
+                {myData.about.currentProject}
+              </RainbowUnderline>
+            </RoughNotationGroup>
+          </Link>
+        </Text>
+      </Container>
+    </Box>
   );
 }
