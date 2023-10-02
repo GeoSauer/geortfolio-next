cbc3791cb9b42993c22d048fad98732eb721605f
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-02 14:37:49 -0600

    added top section of About

--- components/About.js
+++ components/About.js
@@ -8,26 +8,35 @@ export default function About() {
   const bg = useColorModeValue("gray.100", "gray.900");
 
   return (
-    <Box as="section" bg={bg}>
-      <Container maxWidth={"6xl"}>
-        <Heading fontSize={{ base: "6xl", md: "9xl" }} mb={{ base: 20, md: 5 }}>
+    <>
+      <Container maxWidth={"6xl"} h={48}>
+        <Heading
+          fontSize={{ base: "6xl", md: "9xl" }}
+          mb={{ base: 20, md: 5 }}
+          textAlign={{ base: "center", md: "left" }}
+          py={{ base: 5, md: 20 }}
+        >
           About me.
         </Heading>
-        <Text as={"p"} fontSize={{ base: "3xl", md: "4xl" }} fontWeight={"semibold"}>
-          {myData.about.title} Currently working on{" "}
-          <Link
-            _hover={{
-              textDecoration: "none",
-            }}
-          >
-            <RoughNotationGroup show={true}>
-              <RainbowUnderline color={myData.colors[0]}>
-                {myData.about.currentProject}
-              </RainbowUnderline>
-            </RoughNotationGroup>
-          </Link>
-        </Text>
       </Container>
-    </Box>
+      <Box bg={bg} mt={{ base: -20, md: -5 }}>
+        <Container maxWidth={"6xl"} pt={20}>
+          <Text as={"p"} fontSize={{ base: "3xl", md: "4xl" }} fontWeight={"semibold"}>
+            {myData.about.title} Currently working on{" "}
+            <Link
+              _hover={{
+                textDecoration: "none",
+              }}
+            >
+              <RoughNotationGroup show={true}>
+                <RainbowUnderline color={myData.colors[0]}>
+                  {myData.about.currentProject}
+                </RainbowUnderline>
+              </RoughNotationGroup>
+            </Link>
+          </Text>
+        </Container>
+      </Box>
+    </>
   );
 }
