274c8244b0d73e1b7422b3b39cfa99506ad1f809
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-03 16:13:23 -0600

    added project images and links

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PageHeading from "./PageHeading";
 import PageBody from "./PageBody";
-import { SimpleGrid, Link as ChakraLink, Box } from "@chakra-ui/react";
+import { SimpleGrid, Link as ChakraLink, Box, Text } from "@chakra-ui/react";
 import myData from "@/data";
 import Image from "next/image";
 
@@ -11,35 +11,53 @@ export default function Projects() {
       <PageHeading>Projects.</PageHeading>
 
       <PageBody>
-        <SimpleGrid columns={{ base: 1, md: 2 }} pb={20} gap={8}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} pb={20} gap={8} py={{ md: 10 }}>
           {myData.projects?.map((project, idx) => (
-            <ChakraLink key={idx} href={project.link} isExternal>
-              <Box
-                position={"relative"}
-                overflow={"hidden"}
-                shadow={"2xl"}
-                width={"full"}
-                height={"25vh"}
-                rounded={{ md: "lg" }}
-              >
-                <Image
-                  src={project.imageUrl}
-                  alt={project.title}
-                  fill
-                  style={{
-                    objectFit: "cover",
-                    transform: "scale(1)",
-                    transition: "transform 2s ease-out",
-                  }}
-                  onMouseOver={(e) => {
-                    e.currentTarget.style.transform = "scale(1.25)";
-                  }}
-                  onMouseOut={(e) => {
-                    e.currentTarget.style.transform = "scale(1)";
-                  }}
-                />
-              </Box>
-            </ChakraLink>
+            <Fragment key={idx}>
+              <ChakraLink href={project.link} isExternal>
+                <Box
+                  position={"relative"}
+                  overflow={"hidden"}
+                  shadow={"2xl"}
+                  width={"full"}
+                  height={{ base: "27vh", md: "xs" }}
+                  rounded={{ md: "lg" }}
+                >
+                  <Image
+                    src={project.imageUrl}
+                    alt={project.title}
+                    fill
+                    style={{
+                      objectFit: "cover",
+                      transform: "scale(1)",
+                      transition: "transform 2s ease-out",
+                    }}
+                    onMouseOver={(e) => {
+                      e.currentTarget.style.transform = "scale(1.25)";
+                    }}
+                    onMouseOut={(e) => {
+                      e.currentTarget.style.transform = "scale(1)";
+                    }}
+                  />
+                  <Text
+                    as="h1"
+                    fontSize={"2xl"}
+                    fontWeight={"semibold"}
+                    color={"white"}
+                    position={"absolute"}
+                    // ml={{ base: 2, md: -3 }}
+                    // top={-6}
+                    bottom={5}
+                    right={5}
+                    bg={myData.colors[1]}
+                    rounded={"lg"}
+                    px={2}
+                  >
+                    {project.title}
+                  </Text>
+                </Box>
+              </ChakraLink>
+            </Fragment>
           ))}
         </SimpleGrid>
       </PageBody>
