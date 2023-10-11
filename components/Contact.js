151775ee904ae7e43f8463f82f677b274ddd3807
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-11 17:02:03 -0600

    added ContactForm

--- components/Contact.js
+++ components/Contact.js
@@ -1,24 +1,14 @@
 import PageHeading from "./PageHeading";
 import PageBody from "./PageBody";
 import myData from "@/data";
-import {
-  Flex,
-  Link as ChakraLink,
-  Text,
-  VStack,
-  Box,
-  Input,
-  Textarea,
-  FormControl,
-  FormLabel,
-  Container,
-  Button,
-} from "@chakra-ui/react";
+import { Flex, Link as ChakraLink, Text, VStack, Box } from "@chakra-ui/react";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
   const textStyles = {
     as: "p",
     fontSize: { base: "2xl", md: "3xl" },
+    fontWeight: "semibold",
     px: { base: 8, md: 0 },
   };
 
@@ -30,23 +20,17 @@ export default function Contact() {
 
       <PageBody>
         <Flex
-          direction={{ base: "column", md: "row" }}
+          direction={{ base: "column" }}
           sx={textStyles}
           justify={"space-between"}
           mb={{ base: 10, md: 20 }}
         >
-          <Box maxWidth={{ md: "60vw" }} pt={1}>
+          <Box maxWidth={{ md: "60vw" }}>
             <Text fontWeight={"semibold"} fontSize={{ base: "3xl", md: "4xl" }}>
               {myData.contact.title}
             </Text>
           </Box>
-          <VStack align={{ base: "start", md: "end" }} pt={{ base: 10, md: 150 }} spacing={0}>
-            {/* <Text>
-              Send me an{" "}
-              <ChakraLink href="" color={color} isExternal>
-                email.
-              </ChakraLink>
-            </Text> */}
+          <VStack align={{ base: "center", md: "end" }} pt={10}>
             <Text>
               Check out my{" "}
               <ChakraLink href="" color={color} isExternal>
@@ -67,33 +51,8 @@ export default function Contact() {
             </Text>
           </VStack>
         </Flex>
-        <Container
-          maxWidth={"xl"}
-          p={5}
-          align={"center"}
-          bg={"gray.400"}
-          _dark={{ bg: "gray.800" }}
-          rounded={{ md: "lg" }}
-        >
-          <VStack spacing={5}>
-            <Text sx={textStyles}>Get in touch, let&apos;s talk.</Text>
-            <FormControl isRequired>
-              <FormLabel>Your Name</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Message</FormLabel>
-              <Textarea />
-            </FormControl>
-            <FormControl>
-              <Button type="submit">Send Message</Button>
-            </FormControl>
-          </VStack>
-        </Container>
+
+        <ContactForm />
       </PageBody>
     </>
   );
