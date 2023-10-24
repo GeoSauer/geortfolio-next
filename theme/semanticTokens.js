d6af4144c2e0067c05ce4826b1778b194eb108c7
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-24 13:30:33 -0600

    removed unnecessary semanticTokens

--- theme/semanticTokens.js
+++ theme/semanticTokens.js
@@ -4,10 +4,6 @@ const semanticTokens = {
       default: "gray.50",
       _dark: "gray.900",
     },
-    bgSecondary: {
-      default: "yellow.50",
-      _dark: "rgb(0,43,53)",
-    },
     textPrimary: {
       default: "gray.700",
       _dark: "gray.200",
@@ -20,22 +16,6 @@ const semanticTokens = {
       default: "gray.300",
       _dark: "gray.700",
     },
-    icon: {
-      default: "gray.600",
-      _dark: "gray.400",
-    },
-    iconBG: {
-      default: "yellow.100",
-      _dark: "rgb(0,33,43)",
-    },
-    iconHover: {
-      default: "yellow.200",
-      _dark: "teal.800",
-    },
-    divider: {
-      default: "gray.900",
-      _dark: "gray.400",
-    },
     button: {
       default: "gray.100",
       _dark: "gray.100",
@@ -43,22 +23,6 @@ const semanticTokens = {
     buttonText: {
       default: "gray.900",
       _dark: "gray.700",
-    },
-    contactForm: {
-      default: "yellow.50",
-      _dark: "rgb(0,43,53)",
-    },
-    submitButton: {
-      default: "yellow.300",
-      _dark: "teal.700",
-    },
-    submitText: {
-      default: "gray.600",
-      _dark: "gray.400",
-    },
-    submitHover: {
-      default: "yellow.200",
-      _dark: "teal.800",
     },
   },
 };
