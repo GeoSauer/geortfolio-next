fbff8e62c80e69f22c5c736138de52ca342dcf99
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-25 15:13:33 -0600

    added color to PageHeaders and a spooky surprise

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -2,21 +2,17 @@ import { Box, Flex, HStack, Heading } from "@chakra-ui/react";
 import { RoughNotationGroup } from "react-rough-notation";
 import HeroHighlight from "../RoughNotation/HeroHighlight";
 import avatar from "@/public/avatar.png";
+import spookyAvatar from "@/public/spooky-avatar.png";
 import myData from "@/data";
 import CustomImage from "../Custom/CustomImage";
+import { useSuperDarkMode } from "@/providers/SuperDarkModeProvider";
 
 export default function Hero() {
   const headingStyles = { fontSize: { base: "4xl", md: "8xl" } };
+  const { superDarkMode } = useSuperDarkMode();
 
   return (
-    <HStack
-      as={"section"}
-      role="hero"
-      // justify={"center"}
-      align={"start"}
-      // overflow={"hidden"}
-      mt={{ base: -145, md: -350 }}
-    >
+    <HStack as={"section"} role="hero" align={"start"} mt={{ base: -145, md: -350 }}>
       <Flex
         direction={"column"}
         width={{ base: "full", lg: "60vw" }}
@@ -44,7 +40,7 @@ export default function Hero() {
         marginTop={20}
       >
         <CustomImage
-          src={avatar}
+          src={superDarkMode ? spookyAvatar : avatar}
           alt="A photo of Geo"
           priority
           rounded={"45% 55% 74% 26% / 36% 65% 35% 64%"}
--- components/Layout/PageHeading.js
+++ components/Layout/PageHeading.js
@@ -1,6 +1,25 @@
+import myData from "@/data";
 import { Container, Heading } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export default function PageHeading({ children }) {
+  const router = useRouter();
+
+  let color = null;
+  switch (router.asPath) {
+    case "/about":
+      color = myData.colors[0];
+      break;
+    case "/projects":
+      color = myData.colors[1];
+      break;
+    case "/my-journey":
+      color = myData.colors[2];
+      break;
+    case "/contact":
+      color = myData.colors[3];
+  }
+
   return (
     <Container maxWidth={"6xl"} h={48}>
       <Heading
@@ -8,7 +27,7 @@ export default function PageHeading({ children }) {
         mb={{ base: 20, md: 5 }}
         textAlign={{ base: "center", md: "left" }}
         py={{ base: 5, md: 20 }}
-        color="title"
+        color={color}
       >
         {children}
       </Heading>
