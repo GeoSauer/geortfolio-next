20cd5fbdc41544ac7226df2c7351c574333162f8
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-25 12:33:44 -0600

    cleaned up unnecessary bits

--- components/RoughNotation/AboutUnderline.js
+++ components/RoughNotation/AboutUnderline.js
@@ -1,18 +1,17 @@
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotation } from "react-rough-notation";
 
 export default function AboutUnderline({ color, children }) {
   return (
-    <RoughNotationGroup show={true}>
-      <RoughNotation
-        type="underline"
-        padding={[0, 8]}
-        iterations={1}
-        strokeWidth={5}
-        animationDuration={3000}
-        color={color}
-      >
-        {children}
-      </RoughNotation>
-    </RoughNotationGroup>
+    <RoughNotation
+      type="underline"
+      padding={[0, 8]}
+      iterations={1}
+      strokeWidth={5}
+      animationDuration={3000}
+      color={color}
+      show={true}
+    >
+      {children}
+    </RoughNotation>
   );
 }
