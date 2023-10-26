815457d6a1a83454005884477aa38c144ff04586
GeoSauer <geoffrey.sauer89@gmail.com>
2023-10-26 11:53:51 -0600

    refactored About into smaller components

--- components/About/About.js
+++ components/About/About.js
@@ -1,67 +1,12 @@
-import myData from "@/data";
-import { Container, SimpleGrid, Text, Box } from "@chakra-ui/react";
-import AboutUnderline from "../RoughNotation/AboutUnderline";
-import CustomLink from "../Custom/CustomLink";
 import TechStack from "./TechStack";
-
-const subheaderStyles = {
-  fontSize: "3xl",
-  fontWeight: "semibold",
-  color: myData.colors[0],
-  mb: -4,
-  paddingBottom: 10,
-};
+import Bio from "./Bio";
+import AboutTitle from "./AboutTitle";
 
 export default function About() {
   return (
     <>
-      <Text
-        as={"p"}
-        fontSize={{ base: "3xl", md: "4xl" }}
-        fontWeight={"semibold"}
-        px={{ base: 8, md: 0 }}
-      >
-        {myData.about.title} Currently working on{" "}
-        <CustomLink href={myData.about.currentProjectUrl} target="_blank">
-          <AboutUnderline color={myData.colors[0]}>{myData.about.currentProject}</AboutUnderline>
-        </CustomLink>
-      </Text>
-
-      <Container maxWidth={"6xl"} py={20}>
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8} color="textSecondary">
-          <Box>
-            <Text as="h2" sx={subheaderStyles}>
-              Where I&apos;ve been.
-            </Text>
-            {myData.about.desc[0].map((sentence, idx) => (
-              <Text key={idx} fontSize={"2xl"} paddingBottom={2}>
-                {sentence}
-              </Text>
-            ))}
-          </Box>
-          <Box>
-            <Text as="h2" sx={subheaderStyles}>
-              What I&apos;m doing.
-            </Text>
-            {myData.about.desc[1].map((sentence, idx) => (
-              <Text key={idx} fontSize={"2xl"} paddingBottom={2}>
-                {sentence}
-              </Text>
-            ))}
-          </Box>
-          <Box>
-            <Text as="h2" sx={subheaderStyles}>
-              What I hope to do.
-            </Text>
-            {myData.about.desc[2].map((sentence, idx) => (
-              <Text key={idx} fontSize={"2xl"} paddingBottom={2}>
-                {sentence}
-              </Text>
-            ))}
-          </Box>
-        </SimpleGrid>
-      </Container>
-
+      <AboutTitle />
+      <Bio />
       <TechStack />
     </>
   );
---
+++ components/About/Bio.js
@@ -0,0 +1,32 @@
+import myData from "@/data";
+import { Box, Container, SimpleGrid, Text } from "@chakra-ui/react";
+
+const subheaderStyles = {
+  fontSize: "3xl",
+  fontWeight: "semibold",
+  color: myData.colors[0],
+  mb: -4,
+  paddingBottom: 10,
+};
+
+export default function Bio() {
+  return (
+    <Container maxWidth={"6xl"} py={20}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8} color="textSecondary">
+        {myData.about.desc.map((paragraph) => (
+          <Box key={paragraph.title}>
+            <Text as="h2" sx={subheaderStyles}>
+              {paragraph.title}
+            </Text>
+
+            {paragraph.body.map((sentence) => (
+              <Text key={sentence} fontSize="2xl" paddingBottom={2}>
+                {sentence}
+              </Text>
+            ))}
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
+}
