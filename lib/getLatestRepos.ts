8bab61b8be84fe93a1ab47abc0ff06ca15bc18fe
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-07 14:25:09 -0700

    refactored getLastestRepos

---
+++ lib/getLatestRepos.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from "axios";
+
+export type Repositories = {
+  repositories: {
+    name: string;
+    description: string | null;
+    clone_url: string;
+  }[];
+};
+
+const getLatestRepos = async (githubUsername: string, token?: string): Promise<Repositories[]> => {
+  try {
+    const apiUrl = `https://api.github.com/users/${githubUsername}/repos?sort=updated&per_page=6`;
+
+    const headers = token ? { Authorization: `token ${token}` } : {};
+
+    const res: AxiosResponse<Repositories[]> = await axios.get(apiUrl, { headers });
+    const repos: Repositories[] = res.data;
+
+    return repos;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+export default getLatestRepos;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import Layout from "@/components/Layout/Layout";
-import getLatestRepos from "@/lib/getLatestRepos";
 import Home from "@/components/Home/Home";
 import myData from "@/data";
+import getLatestRepos, { Repositories } from "@/lib/getLatestRepos";
 
-type Repos = {
-  repositories: {
-    name: string;
-    description: string | null;
-    clone_url: string;
-  }[];
-};
-
-export default function home({ repositories }: Repos) {
+export default function home({ repositories }: Repositories) {
   return (
     <Layout heading={undefined}>
       <Home repositories={repositories} />
@@ -22,7 +14,7 @@ export default function home({ repositories }: Repos) {
 export const getServerSideProps = async () => {
   //TODO crack Next env variables because this isn't working properly
   let token = process.env.GITHUB_AUTH_TOKEN;
-  const repositories = await getLatestRepos(myData, token);
+  const repositories = await getLatestRepos(myData.githubUsername, token);
 
   return {
     props: {
